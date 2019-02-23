1d8c3c05e642bbda13ced15e17c8293d9deb088f
Mike McAllister <mike@logikbox.com>
2019-02-23 08:38:39 -0500

    removed cookies and converted to localStorage for long term storage

--- javascript/utils.js
+++ javascript/utils.js
@@ -1,4 +1,3 @@
-// Utilities for Asteroids
 
 
 $(window).resize(function () {
@@ -151,42 +150,21 @@ function playExtraLife() {
 
 
 
-// Cookie ---------------------------------------------------------------------------------------
-
-function setCookie(cname, cvalue, exdays) {
-	var d = new Date();
-	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-	var expires = "expires=" + d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-	var name = cname + "=";
-	var ca = document.cookie.split(';');
-	for (var i = 0; i < ca.length; i++) {
-		var c = ca[i];
-		while (c.charAt(0) == ' ') {
-			c = c.substring(1);
-		}
-		if (c.indexOf(name) == 0) {
-			return c.substring(name.length, c.length);
-		}
-	}
-	return "";
-}
+// localStorage ---------------------------------------------------------------------------------------
 
 function checkHighScoreCookie() {
-	var hs = getCookie("highScore");
-	//console.log("hs", hs);
-	if (hs == "") {
-		setCookie('highScore', score, 1000);
+	var hs = localStorage.getItem("highScore");
+
+	if (hs == "" || hs == undefined ) {
+		localStorage.setItem("highScore", score);
 		$('#highScore span').text(score);
 	} else {
 		if (hs <= score) {
-			setCookie('highScore', score, 1000);
+			localStorage.setItem("highScore", score);
 			$('#highScore span').text(score);
 		} else {
 			$('#highScore span').text(hs);
 		}
 	}
+
 }
